c3844412ebb07c6e2f6e30cc44903ddab9bce2cc
weihao <addpd@qq.com>
2024-10-15 21:59:05 +0800

    Site updated: 2024-10-15 21:58:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/15/hello-world/","2024/10/15/☁云服务器/安装宝塔提示/","2024/10/15/☁云服务器/使用七牛云结论：不用/","2024/10/15/☁云服务器/安装开源1panel/","2024/10/15/☁云服务器/halo主题备份/export-theme-hao-config-1720001557998/"];function toRandomPost(){
+var posts=["2024/10/15/hello-world/","2024/10/15/☁云服务器/使用七牛云结论：不用/","2024/10/15/☁云服务器/安装宝塔提示/","2024/10/15/☁云服务器/安装开源1panel/","2024/10/15/☁云服务器/halo主题备份/export-theme-hao-config-1720001557998/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
