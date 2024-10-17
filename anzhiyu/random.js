ad6ad506ef013fc9d5af009d62016d42171d74c3
weihao <addpd@qq.com>
2024-10-17 16:36:26 +0800

    Site updated: 2024-10-17 16:36:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/15/hello-world/"];function toRandomPost(){
+var posts=["2024/10/15/hello-world/","2024/10/16/obsidian_data/💪人体/10个动作，在家练爆全身肌肉!(无器械)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
