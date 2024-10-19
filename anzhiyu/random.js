d860e92c11d7dab694744dff0806081cfe43ab28
weihao <addpd@qq.com>
2024-10-19 13:46:54 +0800

    Site updated: 2024-10-19 13:46:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/15/hello-world/","2024/10/19/obsidian_data/👨‍💻开发记录和电子产品相关/【hexo】使用 lighthouse 优化/","2024/10/16/obsidian_data/👨‍💻开发记录和电子产品相关/【hexo】建立个人blog/","2024/10/16/obsidian_data/💪人体/10个动作，在家练爆全身肌肉!(无器械)/","2024/10/16/obsidian_data/👨‍💻开发记录和电子产品相关/obsidian相关/使用腾讯云cos存储作为图床/"];function toRandomPost(){
+var posts=["2024/10/15/hello-world/","2024/10/16/obsidian_data/💪人体/10个动作，在家练爆全身肌肉!(无器械)/","2024/10/19/obsidian_data/👨‍💻开发记录和电子产品相关/【hexo】使用 lighthouse 优化/","2024/10/16/obsidian_data/👨‍💻开发记录和电子产品相关/【hexo】建立个人blog/","2024/10/16/obsidian_data/👨‍💻开发记录和电子产品相关/obsidian相关/使用腾讯云cos存储作为图床/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
