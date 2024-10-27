00f9b5d03c2179a975ee54d6eedaf7a97f50f703
weihao <addpd@qq.com>
2024-10-27 16:42:10 +0800

    Site updated: 2024-10-27 16:42:09

---
+++ js/reward.js
@@ -0,0 +1,1 @@
+const rewardBtn=document.getElementById("rewardBtn"),rewardImgContainer=document.getElementById("rewardImgContainer");rewardBtn&&(rewardBtn.onclick=()=>{rewardImgContainer.style.display="none"===rewardImgContainer.style.display||""===rewardImgContainer.style.display?"inline-flex":"none"});
