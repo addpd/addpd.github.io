9717670af5aaf18f4d992e8d17512bc2b04049ba
weihao <addpd@qq.com>
2024-10-30 14:59:09 +0800

    1.bootstrap拉取到本地方便去掉无用css。2.reward.js和css移到文章页才加载。

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,18 +1,61 @@
 import gulp from 'gulp';
-import ttf2woff2 from 'gulp-ttf2woff2';
+import cleanCSS from 'gulp-clean-css';
+import htmlmin from 'gulp-htmlmin';
+import htmlclean from 'gulp-htmlclean';
+import babel from 'gulp-babel'; /* 转换为es2015 */
+import uglify from 'gulp-uglify';
 
-gulp.task('ttf2woff2', () => {
-  return gulp // 确保返回流
-    .src(['source/fonts/LXGWWenKaiGBScreen.ttf'], {
-      encoding: false, // Important!
-      removeBOM: false,
-    })
-    .pipe(ttf2woff2())
-    .on('error', function(err) {
-      console.error('Error in ttf2woff2 plugin:', err);
-    })
-    .pipe(gulp.dest('source/fonts/'));
-});
 
-// 默认任务
-gulp.task('default', gulp.series('ttf2woff2'));
+// 设置根目录
+const root = './public'
+
+// 匹配模式， **/*代表匹配所有目录下的所有文件
+const pattern = '**/*'
+
+// 压缩html
+gulp.task('minify-html', function () {
+  return gulp
+    // 匹配所有 .html结尾的文件
+    .src(`${root}/${pattern}.html`)
+    .pipe(htmlclean())
+    .pipe(
+      htmlmin({
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      })
+    )
+    .pipe(gulp.dest('./public'))
+})
+
+// 压缩css
+gulp.task('minify-css', function () {
+  return gulp
+    // 匹配所有 .css结尾的文件
+    .src(`${root}/${pattern}.css`)
+    .pipe(
+      cleanCSS({
+        compatibility: 'ie8'
+      })
+    )
+    .pipe(gulp.dest('./public'))
+})
+
+// 压缩js
+gulp.task('minify-js', function () {
+  return gulp
+    // 匹配所有 .js结尾的文件
+    .src(`${root}/${pattern}.js`)
+    .pipe(
+      babel({
+        // presets: ['env']
+        presets: ['@babel/preset-env']
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest('./public'))
+})
+
+
+gulp.task('default', gulp.parallel('minify-html', 'minify-css', 'minify-js'))
